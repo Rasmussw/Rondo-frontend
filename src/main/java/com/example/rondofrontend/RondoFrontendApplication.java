dd567cc4cb8ef65f80d13f334720dbda5410f3a5
rasmuswoldike <rswoldike@gmail.com>
2022-11-21 14:47:06 +0100

    Initial commit

---
+++ src/main/java/com/example/rondofrontend/RondoFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rondofrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RondoFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RondoFrontendApplication.class, args);
+    }
+
+}
